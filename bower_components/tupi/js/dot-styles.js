f1066c9bffcf45bda1d4fe43d133b6770ff90cab
Daniel Torres <danielfeelfine@gmail.com>
2014-06-02 03:11:54 -0300

    Bump dots.js in bower_components

--- bower_components/tupi/js/dot-styles.js
+++ bower_components/tupi/js/dot-styles.js
@@ -1,13 +1,4 @@
-/**
- * dots.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
+/*jshint undef:false */
 ;( function( window ) {
 
 	'use strict';
@@ -24,8 +15,8 @@
 	function DotNav( el, options ) {
 		this.nav = el;
 		this.options = extend( {}, this.options );
-  		extend( this.options, options );
-  		this._init();
+      extend( this.options, options );
+      this._init();
 	}
 
 	DotNav.prototype.options = {};
@@ -57,7 +48,7 @@
 				}
 			} );
 		} );
-	}
+	};
 
 	// add to global namespace
 	window.DotNav = DotNav;
